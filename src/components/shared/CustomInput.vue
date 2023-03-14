6c3813dd263a560ed96b791e8f4bfa798301911f
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-03-14 19:34:27 +0800

    Create components - CustomInput

---
+++ src/components/shared/CustomInput.vue
@@ -0,0 +1,47 @@
+<template>
+    <input v-on="listeners" class="custom-input">
+</template>
+
+<script>
+export default {
+    name: 'CustomInput',
+    computed: {
+        listeners() {
+            return {
+                ...this.$listeners,
+                input: event => this.$emit('input', event.target.value)
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.custom-input {
+    height: 40px;
+    width: 100%;
+    border: 2px solid orange;
+    font-size: 18px;
+    outline: none;
+    line-height: inherit;
+    padding: 8px 15px;
+
+    /* &::placeholder {
+        color: inherit;
+    } */
+
+    /* &--error {
+        border-color: red;
+    } */
+
+    /* &__error {
+        position: absolute;
+        top: 100%;
+        right: 0;
+        width: 100%;
+        font-size: 12px;
+        color: red;
+        line-height: 1.3;
+    } */
+}
+</style>
