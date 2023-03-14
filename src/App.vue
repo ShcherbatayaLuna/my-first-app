6753c9cb750b59eecf2ef25e4c758c9c857dc7d6
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-03-14 19:41:03 +0800

    Update App

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div :id="$style.app">
+    <h2>{{ text }}</h2>
+    <!-- <input type="text" v-model="text" > -->
+    <!-- <input type="text" :value="text" @input="event => text = event.target.value"> -->
+    <CustomInput v-model="text"></CustomInput>
+    <ApartmentsList v-bind:items="apartments">
+      <template v-slot:title></template>
+      <template v-slot:apartment="{ apartment }">
+        <ApartmentsItem v-bind:key="apartment.id" v-bind:descr="apartment.descr" v-bind:rating="apartment.rating"
+          v-bind:imgSrc="apartment.imgUrl" v-bind:price="apartment.price" @click.native="handleItemClick">
+        </ApartmentsItem>
+      </template>
+    </ApartmentsList>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ApartmentsList from './components/apartments/ApartmentsList.vue';
+import ApartmentsItem from './components/apartments/ApartmentsItem.vue';
+import apartments from './components/apartments/apartments';
+import CustomInput from './components/shared/CustomInput.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ApartmentsList,
+    ApartmentsItem,
+    CustomInput
+  },
+  data() {
+    return {
+      apartments,
+      text: '',
+    }
+  },
+  // computed: {
+  //   title() {
+  //     return `Amount of clicks ${this.amountOfClicks}`
+  //   }
+  // },
+  methods: {
+    handleItemClick() {
+      console.log('item click')
+    }
   }
 }
 </script>
 
-<style>
+<style module>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
