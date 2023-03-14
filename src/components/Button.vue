d1eb16df6bfc2e52e278a98badc202ffff47436d
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-03-14 19:34:47 +0800

    Create components - Button

---
+++ src/components/Button.vue
@@ -0,0 +1,40 @@
+<template>
+    <button v-bind:type="type" v-on="$listeners" :class="{ btn: true, 'btn--outlined': outlined }">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'BtnMain',
+    props: {
+        type: {
+            type: String,
+            default: 'button',
+        },
+        outlined: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+.btn {
+    display: inline-block;
+    font-size: 18px;
+    background: #ff662d;
+    color: white;
+    min-width: 220px;
+    border: 1px solid transparent;
+    padding: 8px 15px;
+    cursor: pointer;
+
+    &--outlined {
+        background: none;
+        border: 1px solid #ff662d;
+        color: #ff662d;
+    }
+}
+</style>
