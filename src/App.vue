0d1479956e43bd70f07c0b1f3dee54270b147dc5
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-03-17 19:54:23 +0800

    Update App (add CustomInput and CustomSelect)

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,13 @@
     <h2>{{ text }}</h2>
     <!-- <input type="text" v-model="text" > -->
     <!-- <input type="text" :value="text" @input="event => text = event.target.value"> -->
+    <CustomSelect :items="['name', 'label', 'salary']"></CustomSelect>
     <CustomInput v-model="text"></CustomInput>
     <ApartmentsList v-bind:items="apartments">
       <template v-slot:title></template>
       <template v-slot:apartment="{ apartment }">
-        <ApartmentsItem v-bind:key="apartment.id" v-bind:descr="apartment.descr" v-bind:rating="apartment.rating"
-          v-bind:imgSrc="apartment.imgUrl" v-bind:price="apartment.price" @click.native="handleItemClick">
+        <ApartmentsItem v-bind:key="apartment.id" :descr="apartment.descr" :rating="apartment.rating"
+          :imgSrc="apartment.imgUrl" :price="apartment.price" @click.native="handleItemClick">
         </ApartmentsItem>
       </template>
     </ApartmentsList>
@@ -19,14 +20,16 @@
 import ApartmentsList from './components/apartments/ApartmentsList.vue';
 import ApartmentsItem from './components/apartments/ApartmentsItem.vue';
 import apartments from './components/apartments/apartments';
-import CustomInput from './components/shared/CustomInput.vue'
+import CustomInput from './components/shared/CustomInput.vue';
+import CustomSelect from './components/shared/CustomSelect.vue';
 
 export default {
   name: 'App',
   components: {
     ApartmentsList,
     ApartmentsItem,
-    CustomInput
+    CustomInput,
+    CustomSelect
   },
   data() {
     return {
