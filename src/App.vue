808ecda33dbc77982e24216b4218245752277861
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-03-17 22:34:55 +0800

    Update App (add filters)

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,10 @@
     <!-- <input type="text" v-model="text" > -->
     <!-- <input type="text" :value="text" @input="event => text = event.target.value"> -->
     <Container>
-      <ApartmentFilterForm class="apartments-filter" @submit="logger"></ApartmentFilterForm>
+      <ApartmentFilterForm class="apartments-filter" @submit="filter"></ApartmentFilterForm>
     </Container>
-    <ApartmentsList v-bind:items="apartments">
+    <p v-if="!filteredApartments.length">Ничего не найдено</p>
+    <ApartmentsList v-else v-bind:items="filteredApartments">
       <template v-slot:apartment="{ apartment }">
         <ApartmentsItem v-bind:key="apartment.id" :descr="apartment.descr" :rating="apartment.rating"
           :imgSrc="apartment.imgUrl" :price="apartment.price" @click.native="handleItemClick">
@@ -35,16 +36,33 @@ export default {
     return {
       apartments,
       text: '',
+      filters: {
+        city: '',
+        price: 0
+      }
     }
   },
-  // computed: {
-  //   title() {
-  //     return `Amount of clicks ${this.amountOfClicks}`
-  //   }
-  // },
+  computed: {
+    filteredApartments() {
+      return this.filterByCityName(this.filterByPrice(this.apartments))
+    }
+  },
   methods: {
-    logger(value) {
-      console.log(value, '---form value')
+    filter({ city, price }) {
+      this.filters.city = city,
+        this.filters.price = price
+    },
+    filterByCityName(apartments) {
+      if (!this.filters.city) return apartments
+      return apartments.filter(apartment => {
+        return apartment.location.city === this.filters.city
+      })
+    },
+    filterByPrice(apartments) {
+      if (!this.filters.price) return apartments
+      return apartments.filter(apartment => {
+        return apartment.price >= this.filters.price
+      })
     }
   }
 }
