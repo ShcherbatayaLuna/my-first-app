fb366ea5227477ab1457941910f1f75a7c0903a9
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-03-23 21:54:46 +0800

    Update App

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,22 @@
 <template>
   <div id="app">
-    <Header />
     <div class="content">
-      <h2>{{ text }}</h2>
-      <!-- <input type="text" v-model="text" > -->
-      <!-- <input type="text" :value="text" @input="event => text = event.target.value"> -->
-      <Container>
-        <ApartmentFilterForm class="apartments-filter" @submit="filter"></ApartmentFilterForm>
-      </Container>
-      <Container>
-        <p v-if="!filteredApartments.length">Ничего не найдено</p>
-        <ApartmentsList v-else v-bind:items="filteredApartments">
-          <template v-slot:apartment="{ apartment }">
-            <ApartmentsItem v-bind:key="apartment.id" :descr="apartment.descr" :rating="apartment.rating"
-              :imgSrc="apartment.imgUrl" :price="apartment.price" @click.native="handleItemClick">
-            </ApartmentsItem>
-          </template>
-        </ApartmentsList>
-      </Container>
+      <Header />
+      <router-view></router-view>
     </div>
     <Footer />
   </div>
 </template>
 
 <script>
-import ApartmentsList from './components/apartments/ApartmentsList.vue';
-import ApartmentsItem from './components/apartments/ApartmentsItem.vue';
-import apartments from './components/apartments/apartments';
-import ApartmentFilterForm from './components/apartments/ApartmentFilterForm.vue';
-import Container from './components/shared/Container.vue';
 import Footer from './components/Footer.vue';
 import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
-    ApartmentsList,
-    ApartmentsItem,
-    ApartmentFilterForm,
-    Container,
-    Footer,
-    Header
-  },
-  data() {
-    return {
-      apartments,
-      text: '',
-      filters: {
-        city: '',
-        price: 0
-      }
-    }
-  },
-  computed: {
-    filteredApartments() {
-      return this.filterByCityName(this.filterByPrice(this.apartments))
-    }
-  },
-  methods: {
-    filter({ city, price }) {
-      this.filters.city = city,
-        this.filters.price = price
-    },
-    filterByCityName(apartments) {
-      if (!this.filters.city) return apartments
-      return apartments.filter(apartment => {
-        return apartment.location.city === this.filters.city
-      })
-    },
-    filterByPrice(apartments) {
-      if (!this.filters.price) return apartments
-      return apartments.filter(apartment => {
-        return apartment.price >= this.filters.price
-      })
-    }
+    Header,
+    Footer
   }
 }
 </script>
@@ -91,9 +34,5 @@ export default {
 .content {
   flex-grow: 1;
   padding-top: 120px;
-}
-
-.apartments-filter {
-  margin-bottom: 40px;
 }
 </style>
