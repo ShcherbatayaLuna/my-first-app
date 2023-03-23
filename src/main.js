107083dac24af02712090069dab73493e88c9236
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-03-23 21:55:35 +0800

    Update main.js (add router)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App.vue";
+import router from "./router";
 import "./assets/scss/index.scss";
 
 Vue.config.productionTip = false;
+Vue.use(VueRouter);
 
 new Vue({
   render: (h) => h(App),
+  router,
 }).$mount("#app");
