93dbbca8e8b4efc5975fa7950da8ac7d7992c78c
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-03-23 22:31:06 +0800

    Update ApartmentPage (add components mainInfo and owner)

--- src/pages/ApartmentPage.vue
+++ src/pages/ApartmentPage.vue
@@ -1,24 +1,57 @@
 <template>
-    <div>
+    <main class="apartment-page">
         <Container>
-            <h1>Apartment Page</h1>
+            <div class="apartment-page__content">
+                <ApartmentsMainInfo :apartment="apartment" />
+                <ApartmentOwner class="apartment-page__owner" :owner="apartment.owner" />
+            </div>
         </Container>
-    </div>
+    </main>
 </template>
 
 <script>
 import Container from '../components/shared/Container.vue';
+import apartments from '../components/apartments/apartments';
+import ApartmentsMainInfo from '../components/apartments/ApartmentsMainInfo.vue';
+import ApartmentOwner from '../components/apartments/ApartmentOwner.vue';
 
 export default {
     name: 'ApartmentPage',
     components: {
-        Container
+        Container,
+        ApartmentsMainInfo,
+        ApartmentOwner
+    },
+    computed: {
+        apartment() {
+            return apartments.find(apartment => apartment.id === this.$route.params.id)
+        }
     },
     mounted() {
-        // console.log(this.$route.params.id)
-        // console.log(this.$route.query.name)
+        console.log(this.apartment)
     }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.apartment-page {
+    padding-bottom: 55px;
+
+    &__content {
+        display: flex;
+        align-items: flex-start;
+    }
+
+    &__additional-info {
+        margin-left: 30px;
+        max-width: 350px;
+        flex-grow: 0;
+        flex-shrink: 1;
+    }
+
+    &__owner {
+        max-width: 350px;
+        margin-left: 30px;
+    }
+}
+</style>
