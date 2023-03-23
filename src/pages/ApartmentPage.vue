7717b294f7df3f478878cdf05995ab9012e9102a
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-03-23 21:55:55 +0800

    Create ApartmentPage

---
+++ src/pages/ApartmentPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Container>
+            <h1>Apartment Page</h1>
+        </Container>
+    </div>
+</template>
+
+<script>
+import Container from '../components/shared/Container.vue';
+
+export default {
+    name: 'ApartmentPage',
+    components: {
+        Container
+    },
+    mounted() {
+        // console.log(this.$route.params.id)
+        // console.log(this.$route.query.name)
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
