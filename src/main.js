b9f04790d1545fc25c1657f65cd41577b7963be7
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-04-04 19:50:51 +0800

    Update main (add Notifications with vue-notifications)

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./App.vue";
 import router from "./router";
+import Notifications from "vue-notification";
 import "./assets/scss/index.scss";
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
+Vue.use(Notifications);
 
 new Vue({
   render: (h) => h(App),
