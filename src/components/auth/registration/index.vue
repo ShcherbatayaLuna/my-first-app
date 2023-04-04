b6e99448fc6996b2236cbb4067192b4c5d0328e2
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-04-04 18:59:37 +0800

    Create registrationForm

---
+++ src/components/auth/registration/index.vue
@@ -0,0 +1,112 @@
+<template>
+    <AuthContainer>
+        <MainTitle class="registration__title">Регистрация</MainTitle>
+        <Form ref="form" class="registration__form" @submit.prevent="handleSubmit">
+            <CustomInput v-model="formData.name" name="name" placeholder="Name" autocomplete="username" :rules="nameRules"
+                class="registration__input" />
+            <CustomInput v-model="formData.email" name="email" placeholder="Email" autocomplete="email" :rules="emailRules"
+                class="registration__input" />
+            <CustomInput v-model="formData.password" name="password" placeholder="Password" autocomplete="current password"
+                type="password" :rules="passwordRules" class="registration__input" />
+            <CustomInput v-model="formData.confirmPassword" name="confirmPassword" placeholder="Confirm password"
+                autocomplete="current password" type="password" :rules="confirmPassword" class="registration__input" />
+            <Button type="submit" class="registration__btn">Вход</Button>
+        </Form>
+    </AuthContainer>
+</template>
+
+<script>
+import Form from '../../shared/form';
+import CustomInput from '../../shared/CustomInput.vue';
+import Button from '../../shared/Button.vue';
+import AuthContainer from '../AuthContainer.vue';
+import MainTitle from '@/components/shared/MainTitle.vue';
+import { emailValidation, passwordValidation, isRequired } from '@/utils/validationRules';
+import { registerUser } from '@/services/auth.service';
+
+export default {
+    name: 'RegistrationForm',
+    components: {
+        Form,
+        CustomInput,
+        Button,
+        AuthContainer,
+        MainTitle
+    },
+    data() {
+        return {
+            formData: {
+                name: '',
+                email: '',
+                password: '',
+                confirmPassword: '',
+            }
+        }
+    },
+    computed: {
+        rules() {
+            return {
+                emailValidation,
+                passwordValidation,
+                isRequired
+            }
+        },
+        nameRules() {
+            return [this.rules.isRequired];
+        },
+        emailRules() {
+            return [this.rules.isRequired, this.rules.emailValidation];
+        },
+        passwordRules() {
+            return [this.rules.isRequired, this.rules.passwordValidation];
+        },
+        confirmPassword() {
+            return [(val) => ({
+                hasPassed: val === this.formData.password,
+                message: 'Пароли не совпадают'
+            })]
+        }
+    },
+    methods: {
+        async handleSubmit() {
+            const { form } = this.$refs
+            const isFormValid = form.validate()
+            const { name, password, email } = this.formData
+
+            if (isFormValid) {
+                try {
+                    const { data } = await registerUser({ name, password, email });
+                    console.log(data);
+                    form.reset();
+                } catch (error) {
+                    console.error(error);
+                }
+
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.registration {
+    &__form {
+        display: block;
+        flex-direction: column;
+    }
+
+    &__title {
+        text-align: center;
+    }
+
+    &__input {
+        margin-bottom: 20px;
+        width: 100%;
+    }
+
+    &__btn {
+        margin-top: 15px;
+        width: 100%;
+    }
+}
+</style>
