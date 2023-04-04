d487bc9705533e84eb0dac6b050f338e8025a8fa
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-04-04 18:15:05 +0800

    Folder 'login' moved in 'auth'

---
+++ src/components/auth/login/index.vue
@@ -0,0 +1,94 @@
+<template>
+    <AuthContainer>
+        <MainTitle class="login__title">Логин</MainTitle>
+        <Form ref="form" class="login__form" @submit.prevent="handleSubmit">
+            <CustomInput v-model="formData.email" name="email" placeholder="Email" :rules="emailRules"
+                class="login__input" />
+            <CustomInput v-model="formData.password" name="password" placeholder="Password" type="password"
+                :rules="passwordRules" class="login__input" />
+            <Button type="submit" class="login__btn">Вход</Button>
+        </Form>
+    </AuthContainer>
+</template>
+
+<script>
+import Form from '../../shared/form';
+import CustomInput from '../../shared/CustomInput.vue';
+import Button from '../../shared/Button.vue';
+import AuthContainer from '../AuthContainer.vue';
+import MainTitle from '@/components/shared/MainTitle.vue';
+import { emailValidation, passwordValidation, isRequired } from '@/utils/validationRules';
+import { loginUser } from '@/services/auth.service';
+
+export default {
+    name: 'LoginForm',
+    components: {
+        Form,
+        CustomInput,
+        Button,
+        AuthContainer,
+        MainTitle
+    },
+    data() {
+        return {
+            formData: {
+                email: '',
+                password: ''
+            }
+        }
+    },
+    computed: {
+        rules() {
+            return {
+                emailValidation,
+                passwordValidation,
+                isRequired
+            }
+        },
+        emailRules() {
+            return [this.rules.isRequired, this.rules.emailValidation];
+        },
+        passwordRules() {
+            return [this.rules.isRequired];
+        },
+    },
+    methods: {
+        async handleSubmit() {
+            const isFormValid = this.$refs.form.validate()
+
+            if (isFormValid) {
+                try {
+                    const { data } = await loginUser(this.formData)
+                    console.log(data)
+                } catch (error) {
+                    console.error(error)
+                }
+
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.login {
+    &__form {
+        display: block;
+        flex-direction: column;
+    }
+
+    &__title {
+        text-align: center;
+    }
+
+    &__input {
+        margin-bottom: 20px;
+        width: 100%;
+    }
+
+    &__btn {
+        margin-top: 15px;
+        width: 100%;
+    }
+}
+</style>
