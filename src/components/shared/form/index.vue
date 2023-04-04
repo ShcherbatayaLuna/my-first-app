2f05f3e4c9767c7e0432be4e071a7965e936ad25
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-04-04 17:27:38 +0800

    Create CustomForm

---
+++ src/components/shared/form/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <form v-on="$listeners" class="form">
+        <slot></slot>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'Form',
+    provide() {
+        return {
+            form: this
+        }
+
+    },
+    data() {
+        return {
+            inputs: []
+        }
+    },
+    methods: {
+        registerInput(input) {
+            this.inputs.push(input)
+        },
+        unRegisterInput(input) {
+            this.inputs.filter(item => item !== input)
+        },
+        validate() {
+            return this.inputs.reduce((isValid, input) => {
+                const isInputValid = input.validate()
+                console.log(isValid, 'acc')
+                console.log(isInputValid, 'input')
+                return isValid && isInputValid
+            }, true)
+        },
+        reset() {
+            return this.inputs.forEach(input => input.reset())
+        }
+    }
+}
+</script>
