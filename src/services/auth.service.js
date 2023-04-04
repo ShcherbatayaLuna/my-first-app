16908154fd46c1613227d126eafeeae5e4ad8595
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-04-04 18:59:24 +0800

    Update auth.service.js (add registerUser)

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,3 +3,7 @@ import axiosInstance from "../utils/axios";
 export const loginUser = (payload) => {
   return axiosInstance.post("/users/login", payload);
 };
+
+export const registerUser = (payload) => {
+  return axiosInstance.post("/users/register", payload);
+};
