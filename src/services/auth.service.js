b1514d563d25dc6255e2a9fe1ba5654ea9da973a
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-04-11 00:05:49 +0800

    Update auth.service (add logout)

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -7,3 +7,7 @@ export const loginUser = (payload) => {
 export const registerUser = (payload) => {
   return axiosInstance.post("/users/register", payload);
 };
+
+export const logout = () => {
+  return axiosInstance.post("/users/logout");
+};
