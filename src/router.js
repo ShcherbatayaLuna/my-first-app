33186f824f41f55382e8f801c3ba4bc9cb8fefab
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-04-11 14:58:18 +0800

    Update router(lazy loading)

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
-import HomePage from "./pages/HomePage.vue";
-import ApartmentPage from "./pages/ApartmentPage.vue";
 import VueRouter from "vue-router";
-import ErrorPage from "./pages/ErrorPage.vue";
-import LoginPage from "./pages/LoginPage.vue";
-import RegistrationPage from "./pages/RegistrationPage.vue";
-import MyOrdersPage from "./pages/MyOrdersPage.vue";
+import store from "./store";
+
+const HomePage = () => import("./pages/HomePage.vue");
+const ApartmentPage = () => import("./pages/ApartmentPage.vue");
+const LoginPage = () => import("./pages/LoginPage.vue");
+const RegistrationPage = () => import("./pages/RegistrationPage.vue");
+const MyOrdersPage = () => import("./pages/MyOrdersPage.vue");
+const ErrorPage = () => import("./pages/ErrorPage.vue");
 
 const routes = [
   {
@@ -16,21 +18,33 @@ const routes = [
     path: "/apartments/:id",
     component: ApartmentPage,
     name: "apartment",
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/login",
     component: LoginPage,
     name: "login-page",
+    meta: {
+      hideForAuth: true,
+    },
   },
   {
     path: "/registration",
     component: RegistrationPage,
     name: "registration-page",
+    meta: {
+      hideForAuth: true,
+    },
   },
   {
     path: "/my-orders",
     component: MyOrdersPage,
     name: "my-orders",
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "*",
@@ -40,5 +54,23 @@ const routes = [
 ];
 
 const router = new VueRouter({ routes, mode: "history" });
+
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = store.getters["auth/isLoggedIn"];
+
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!isLoggedIn) {
+      next({ name: "login-page" });
+    }
+  }
+
+  if (to.matched.some((record) => record.meta.hideForAuth)) {
+    if (isLoggedIn) {
+      next({ name: "homepage" });
+    }
+  }
+
+  next();
+});
 
 export default router;
