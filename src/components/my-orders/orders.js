eb9e3d0794eca6a191bfd66a6aec79724f2088f9
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-04-11 15:06:44 +0800

    Create orders.js

---
+++ src/components/my-orders/orders.js
@@ -0,0 +1,184 @@
+export default [
+  {
+    id: "5f689a2c09b16000e9a2f47b",
+    title: "Delectus et iste.",
+    imgUrl: "/img/apartments/apartment-1.jpeg",
+    price: 1667,
+    location: {
+      city: "Dnipro",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f47d",
+    title: "Eaque suscipit incidunt.",
+    imgUrl: "/img/apartments/apartment-2.jpeg",
+    price: 2740,
+    location: {
+      city: "Poltava",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f48a",
+    title: "Doloremque labore ullam molestiae quis quis eos consequatur.",
+    imgUrl: "/img/apartments/apartment-3.jpeg",
+    price: 2153,
+    location: {
+      city: "Kharkiv",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f481",
+    title: "Dolor at inventore et voluptas omnis unde.",
+    imgUrl: "/img/apartments/apartment-4.jpeg",
+    price: 3074,
+    location: {
+      city: "Poltava",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f47c",
+    title: "Sapiente sit consequuntur odit reprehenderit earum maxime.",
+    imgUrl: "/img/apartments/apartment-5.jpeg",
+    price: 2642,
+    location: {
+      city: "Mariupol",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f47f",
+    title: "Ut accusantium atque at aut aliquid distinctio corporis.",
+    imgUrl: "/img/apartments/apartment-6.jpeg",
+    price: 768,
+    location: {
+      city: "Mariupol",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f48c",
+    title:
+      "Sit similique cumque dolorem ut voluptatibus velit et dicta architecto.",
+    imgUrl: "/img/apartments/apartment-7.jpeg",
+    price: 2642,
+    location: {
+      city: "Odesa",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f485",
+    title: "Nihil pariatur eum ab a ducimus nam.",
+    imgUrl: "/img/apartments/apartment-8.jpeg",
+    price: 3959,
+    location: {
+      city: "Kharkiv",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f48b",
+    title: "Adipisci voluptates accusamus officia aut id et ut hic.",
+    imgUrl: "/img/apartments/apartment-9.jpeg",
+    price: 1165,
+    location: {
+      city: "Kyiv",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f47e",
+    title: "Rerum ut consequatur et adipisci.",
+    imgUrl: "/img/apartments/apartment-10.jpeg",
+    price: 1568,
+    location: {
+      city: "Kyiv",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f488",
+    title: "Aut excepturi sed reiciendis.",
+    imgUrl: "/img/apartments/apartment-11.jpeg",
+    price: 1555,
+    location: {
+      city: "Odesa",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f487",
+    title: "Eligendi vel voluptates perspiciatis.",
+    imgUrl: "/img/apartments/apartment-12.jpeg",
+    price: 652,
+    location: {
+      city: "Dnipro",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f480",
+    title: "Quis nobis eius sint occaecati.",
+    imgUrl: "/img/apartments/apartment-13.jpeg",
+    price: 3530,
+    location: {
+      city: "Mariupol",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f479",
+    title: "Voluptatibus ea consectetur sed sed quas saepe quo.",
+    imgUrl: "/img/apartments/apartment-14.jpeg",
+    price: 3250,
+    location: {
+      city: "Kherson",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f47a",
+    title: "Tempora laudantium quos sed.",
+    imgUrl: "/img/apartments/apartment-15.jpeg",
+    price: 3771,
+    location: {
+      city: "Odesa",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f482",
+    title: "Autem saepe est aperiam tempora.",
+    imgUrl: "/img/apartments/apartment-16.jpeg",
+    price: 2249,
+    location: {
+      city: "Kyiv",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f484",
+    title: "Qui omnis magni ad omnis neque.",
+    imgUrl: "/img/apartments/apartment-17.jpeg",
+    price: 2324,
+    location: {
+      city: "Dnipro",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f489",
+    title: "Possimus iste error harum eum sunt rerum perspiciatis nisi.",
+    imgUrl: "/img/apartments/apartment-18.jpeg",
+    price: 605,
+    location: {
+      city: "Lviv",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f486",
+    title: "Saepe autem quam rerum similique enim rerum molestias.",
+    imgUrl: "/img/apartments/apartment-19.jpeg",
+    price: 2330,
+    location: {
+      city: "Mariupol",
+    },
+  },
+  {
+    id: "5f689a2c09b16000e9a2f483",
+    title:
+      "Ipsum maxime molestiae harum voluptas beatae omnis neque consectetur.",
+    imgUrl: "/img/apartments/apartment-20.jpeg",
+    price: 1099,
+    location: {
+      city: "Lviv",
+    },
+  },
+];
