97e5c695951320f810c3dac58dba1ee69d5bd996
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-04-11 13:45:51 +0800

    Update order.service.js (add getOrder)

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -3,3 +3,7 @@ import axiosInstance from "../utils/axios";
 export const bookApartment = (payload) => {
   return axiosInstance.post("/orders", payload);
 };
+
+export const getOrders = () => {
+  return axiosInstance.get("/orders");
+};
